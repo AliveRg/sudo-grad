76577d3d3ff91ad9e0ced78a85b3b41fec3d2fc4
AliveRg <renatgorodov49280@gmail.com>
2023-02-26 00:14:52 +0300

    DataBase Settins first Table and Seeder and model

---
+++ database/seeders/ProductTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $products = [
+            [
+                'product_id' => '1',
+                'title' => 'Business Law',
+                'content' => 'There are many variations of passages
+                of Lorem Ipsum available, but majority 
+                going to use a passage.',
+                'price'=> '80$',
+                'image_path' => 'Empty',
+            ],
+            [ 
+
+                'product_id' => '2',
+                'title' => 'Construction Law',
+                'content' => 'There are many variations of passages
+                of Lorem.',
+                'price'=> '90$',
+                'image_path' => 'Empty',
+
+            ]
+        ];
+
+        foreach($products as $key => $value) {
+            Product::create($value);
+        }
+        // это почему-то не работает. php artisan db:seed 
+
+    }
+}
