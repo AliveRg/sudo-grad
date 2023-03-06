97874a75d99f4f52d5f7c61f964c0b77e55c62ab
AliveRg <renatgorodov49280@gmail.com>
2023-03-06 20:37:47 +0300

    add table cases

---
+++ app/Http/Controllers/CasesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\ProductCases;
+use Inertia\Inertia;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+
+class CasesController extends Controller
+{
+    //
+}
