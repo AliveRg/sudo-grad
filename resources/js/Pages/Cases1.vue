48ea4f6ba26f82da92e045f71d2dc1ea6b144745
AliveRg <renatgorodov49280@gmail.com>
2023-03-13 12:24:38 +0300

    test add page Cases1 in Cases

---
+++ resources/js/Pages/Cases1.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from "vue";
+import Cases from "@/Pages/Cases.vue";
+import { Head } from "@inertiajs/vue3";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// swiper bundle styles
+import "swiper/swiper-bundle.min.css";
+
+// swiper core styles
+import "swiper/swiper.min.css";
+
+// modules styles
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination, Navigation } from "swiper";
+</script>
+
+<template>
+    <Cases>
+        <p class="text-1">test</p>
+    </Cases>
+</template>
+
+<script>
+SwiperCore.use([Pagination, Navigation]);
+
+export default {
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    setup() {
+        const onSwiper = (swiper) => {
+            console.log(swiper);
+        };
+        const onSlideChange = () => {
+            console.log("slide change");
+        };
+        return {
+            onSwiper,
+            onSlideChange,
+            modules: [Navigation, Pagination, Scrollbar, A11y],
+        };
+    },
+
+    props: {
+        products: Object,
+        cases: Object,
+    },
+    name: "Cases1",
+};
+</script>
